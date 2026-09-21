6b0e8d3f6712d8fc89cea2ee6a6ff59cd046039e
opficdev <162981733+opficdev@users.noreply.github.com>
2026-09-21 11:38:34 +0900

    chore: SwiftLint 구성 추가

--- Tests/UIComposableCompileFailureFixtures/NonisolatedComposableView.swift
+++ Tests/UIComposableCompileFailureFixtures/NonisolatedComposableView.swift
@@ -4,6 +4,6 @@ import UIComposable
 @MainActor
 private final class UIComposableFixtureView: UIView, UIComposable {}
 
-private func MainActor_격리_밖에서는_composable을_사용할_수_없다(_ view: UIComposableFixtureView) {
+private func mainActor_격리_밖에서는_composable을_사용할_수_없다(_ view: UIComposableFixtureView) {
     _ = view.composable()
 }
